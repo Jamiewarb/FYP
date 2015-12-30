728fb5887537f064b932f240b2b902fb332a5743
Jamie Warburton <jw715@bath.ac.uk>
2015-12-30 22:13:51 +0000

    Beginning deliverable creation - website design

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# FYP
+#FYP
+
+#Ideas
+CloudFlare
+SASS/LESS
+Instructions for creating initial database for testing
+Instructions for editing via SASS/LESS
---
+++ Source/home.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>	
+	<head>
+
+		<!-- Meta, title, CSS, favicons, etc. -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Shnip.it - Efficient Cross-Project Snippet Reuse Tool</title>
+
+		<!-- Bootstrap core CSS -->
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+		<link rel="stylesheet" href="resources/css/main.css">
+		<link rel="stylesheet" href="resources/css/home.css">
+
+	</head>
+	
+	<body>
+	
+		<nav class="navbar navbar-default" role="navigation">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#nav-col">
+			      <span class="sr-only">Toggle navigation</span>
+			      <span class="icon-bar"></span>
+			      <span class="icon-bar"></span>
+			      <span class="icon-bar"></span>
+			    </button>
+			    <a class="navbar-brand" href="#">Shnip.it </a>
+			  </div>
+			  
+			  <div class="collapse navbar-collapse" id="nav-col">
+			    <form class="navbar-form navbar-left" role="search">
+			      <div class="form-group">
+			        <input type="text" class="form-control" placeholder="Search">
+			      </div>
+			      <button type="submit" class="btn btn-default">Go!</button>
+			    </form>
+			    <ul class="nav navbar-nav navbar-right">
+			      <li class="dropdown">
+			        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Boards <b class="caret"></b></a>
+			        <ul class="dropdown-menu">
+			          <li><a href="#">My Boards</a></li>
+			          <li><a href="#">Public Boards</a></li>
+			          <!--<li class="divider"></li>-->
+			        </ul>
+			      </li>
+			      <li class="dropdown">
+			        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Create <b class="caret"></b></a>
+			        <ul class="dropdown-menu">
+			          <li><a href="#">A Snippet</a></li>
+			          <li><a href="#">A Board</a></li>
+			        </ul>
+			      </li>
+			      <li class="dropdown">
+			        <a href="#" class="dropdown-toggle" data-toggle="dropdown">User <b class="caret"></b></a>
+			        <ul class="dropdown-menu">
+			          <li><a href="#">Login</a></li>
+			          <li><a href="#">Register</a></li>
+			        </ul>
+			      </li>
+			      <li class="dropdown">
+			        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Help <b class="caret"></b></a>
+			        <ul class="dropdown-menu">
+			          <li><a href="#">FAQ</a></li>
+			          <li><a href="#">Contact</a></li>
+			        </ul>
+			      </li>
+			      <li class="dropdown">
+			        <a href="#" class="dropdown-toggle" data-toggle="dropdown">About <b class="caret"></b></a>
+			        <ul class="dropdown-menu">
+			          <li><a href="#">Project</a></li>
+			          <li><a href="#">The Team</a></li>
+			        </ul>
+			      </li>
+			    </ul>
+			  </div><!-- /.navbar-collapse -->
+			</div><!-- /.container-fluid -->
+		</nav>
+	
+		<!-- JS and analytics only. -->
+    <!-- Bootstrap core JavaScript
+		================================================== -->
+		<!-- Placed at the end of the document so the pages load faster -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.js"></script>
+	
+	</body>
+</html>
