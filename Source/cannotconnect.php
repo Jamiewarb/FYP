899a7fc5c24b5f0c25d5f0fccbf4a182184718eb
Jamie Warburton <jw715@bath.ac.uk>
2016-05-21 17:00:33 +0100

    Added missing source files

---
+++ Source/cannotconnect.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Error - Couldn't connect to Database</title>
+</head>
+<body>
+Error - We were unable to connect to the database.
+
+Please ensure the MYSQL server is running.
+
+Contact jw715@bath.ac.uk if problems persist.
+</body>
+</html>
